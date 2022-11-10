b180eb1ae10ef321c73ede1e034a066afd44c645
Johnny Hoang <johnny.hoang@dawsoncollege.qc.ca>
2022-11-10 12:55:36 -0500

    Add project dependencies

--- src/main/java/com/mycompany/mqtt/client/app/MqttRun.java
+++ src/main/java/com/mycompany/mqtt/client/app/MqttRun.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.mycompany.mqttlab;
+package com.mycompany.mqtt.client.app;
 import com.hivemq.client.mqtt.MqttClient;
 import com.hivemq.client.mqtt.mqtt5.Mqtt5BlockingClient;
 
