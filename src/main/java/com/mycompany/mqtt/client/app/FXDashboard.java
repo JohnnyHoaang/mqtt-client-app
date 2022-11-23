dafd23fa96e4ae9859163e48325bc098f4b9c65c
Katharina <katharina.orfanidis@dawsoncollege.qc.ca>
2022-11-22 19:38:39 -0500

    Modify text tile

--- src/main/java/com/mycompany/mqtt/client/app/FXDashboard.java
+++ src/main/java/com/mycompany/mqtt/client/app/FXDashboard.java
@@ -4,8 +4,11 @@ import java.io.IOException;
 
 import eu.hansolo.tilesfx.Tile;
 import eu.hansolo.tilesfx.TileBuilder;
+import eu.hansolo.tilesfx.Tile.SkinType;
 import javafx.application.Platform;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Stop;
@@ -27,6 +30,7 @@ public class FXDashboard extends HBox {
             .unit("Celsius")
             .textVisible(true)
             .value(0)
+            .threshold(75)
             .gradientStops(new Stop(0, Tile.BLUE),
                            new Stop(0.25, Tile.GREEN),
                            new Stop(0.5, Tile.YELLOW),
@@ -45,22 +49,60 @@ public class FXDashboard extends HBox {
             .maxValue(60)
             .build();
 
-        //Setup a tile with an exit button to end the application
-        var exitButton = new Button("Exit");
+        //Setup tile with TextArea to display output from external program
+        TextArea textArea = new TextArea();
 
-        //Setup event handler for the exit button
-        exitButton.setOnAction(e -> endApplication());
+        //Make the TextArea non editable
+        textArea.setEditable(false);
 
-        var exitTile = TileBuilder.create()
-            .skinType(Tile.SkinType.CUSTOM)
-            .prefSize(PREF_WIDTH, PREF_HEIGHT)
-            .textSize(Tile.TextSize.BIGGER)
-            .title("Quit the application")
-            .graphic(exitButton)
-            .roundedCorners(false)
-            .build();
+        /*Change the background and the font color of the TextArea
+           and make the border of the TextArea transparent
+         */
+        textArea.setStyle("-fx-control-inner-background: #2A2A2A; "
+                + "-fx-text-inner-color: white;"
+                + "-fx-text-box-border: transparent;");
+
+        //Write output to TextArea
+        textArea.setText("\n\nOUTPUT");
+
+        //Layout to contain the TextArea
+        VBox textAreaVbox = new VBox(textArea);
+
+        // //Setup the tile
+        // var textAreaTile = TileBuilder.create()
+        //         .skinType(Tile.SkinType.CUSTOM)
+        //         .prefSize(PREF_WIDTH, PREF_HEIGHT)
+        //         .textSize(Tile.TextSize.BIGGER)
+        //         .title("Member 1 Buzzer")
+        //         .graphic(textAreaVbox)
+        //         .build();
+
+        var textTile = TileBuilder.create()
+                .skinType(SkinType.TEXT)
+                .prefSize(PREF_WIDTH, PREF_HEIGHT)
+                .title("Member 1 Buzzer")
+                .text("TimeStamp when buzzer is pressed")
+                .description("Timstamp:")
+                .descriptionAlignment(Pos.BASELINE_LEFT)
+                .textVisible(true)
+                .build();
+
+        // //Setup a tile with an exit button to end the application
+        // var exitButton = new Button("Exit");
+
+        // //Setup event handler for the exit button
+        // exitButton.setOnAction(e -> endApplication());
+
+        // var exitTile = TileBuilder.create()
+        //     .skinType(Tile.SkinType.CUSTOM)
+        //     .prefSize(PREF_WIDTH, PREF_HEIGHT)
+        //     .textSize(Tile.TextSize.BIGGER)
+        //     .title("Quit the application")
+        //     .graphic(exitButton)
+        //     .roundedCorners(false)
+        //     .build();
         
-        var tilesColumn1 = new VBox(gaugeTile, percentageTile, exitTile);
+        var tilesColumn1 = new VBox(gaugeTile, percentageTile, textTile);
         tilesColumn1.setMinWidth(PREF_WIDTH);
         tilesColumn1.setSpacing(5);
 
