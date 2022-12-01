9ad53b9f99160df11e7806696d2dda53d96e9aa8
alex efstathakis <alexprice02@hotmail.com>
2022-12-01 12:54:41 -0500

    working on encoding pictures

--- src/main/java/com/mycompany/mqtt/client/app/MotionSensorApp.java
+++ src/main/java/com/mycompany/mqtt/client/app/MotionSensorApp.java
@@ -2,9 +2,9 @@ package com.mycompany.mqtt.client.app;
 
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.Base64;
-
-import org.apache.commons.io.FileUtils;
 import org.json.JSONObject;
 
 import com.hivemq.client.mqtt.mqtt5.Mqtt5BlockingClient;
@@ -43,9 +43,15 @@ public class MotionSensorApp extends Sensor{
     }
 
     @Override
-    public void sendSensorData(String topic){
-        byte[] fileContent = FileUtils.readFileToByteArray(new File(camera.getOutPutPath() + camera.getCameraCount()));
-        String encodedString = Base64.getEncoder().encodeToString(fileContent);
+    public void sendSensorData(String topic) throws IOException{
+        //byte[] fileContent = FileUtils.readFileToByteArray(new File(camera.getOutPutPath() + camera.getCameraCount()));
+        File f = new File(camera.getOutPutPath() + camera.getCameraCount());
+        FileInputStream fin = new FileInputStream(f);
+        byte imageByteArray[] = new byte[(int)f.length()];
+        fin.read(imageByteArray);
+        String encodedString = Base64.getEncoder().encodeToString(imageByteArray);
+        fin.close();
+        //String encodedString = Base64.getEncoder().encodeToString(fileContent);
         JSONObject jsonMessage = new JSONObject();
         jsonMessage.put("pic",encodedString);
         getMqtt().publishMessage(getClient(), topic, jsonMessage.toString().getBytes());
