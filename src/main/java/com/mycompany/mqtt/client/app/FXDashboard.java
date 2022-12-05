e8b81a22f8c8a77b4ed249ab501ae6b89a7fdf15
johnnyhoaang <johnny.hoang@dawsoncollege.qc.ca>
2022-12-05 00:21:35 -0500

    Separate data logic from FXDashboard

--- src/main/java/com/mycompany/mqtt/client/app/FXDashboard.java
+++ src/main/java/com/mycompany/mqtt/client/app/FXDashboard.java
@@ -42,6 +42,7 @@ public class FXDashboard extends HBox {
     private int PREF_WIDTH = 300;
     private int PREF_HEIGHT = 200;
     private HashMap storedUsersPublicKeys = new HashMap<String, PublicKey>();
+    private DataHandler dataInstance;
     Console console = System.console();
 
     public FXDashboard(MqttRun mqtt, String topicUser) {
@@ -49,10 +50,10 @@ public class FXDashboard extends HBox {
         String ksPath = console.readLine("Enter Keystore path: ");
         char[] password = console.readPassword("Enter Keystore password: ");
         var ks = this.instance.loadKeystore(ksPath, password);
-        this.mqtt = mqtt;
         mqtt.setKeyStore(ks);
+        this.mqtt = mqtt;
         this.client = mqtt.run();
-
+        this.dataInstance = new DataHandler(mqtt, client, storedUsersPublicKeys, this);
         try {
             this.buildScreen();
         } catch (IOException e) {
@@ -181,7 +182,7 @@ public class FXDashboard extends HBox {
         }
         this.setSpacing(5);
 
-        retrieveData();
+        dataInstance.retrieveData();;
     }
 
     /**
@@ -214,161 +215,13 @@ public class FXDashboard extends HBox {
             tiles.add(buzzer);
         }
     }
-
-    /**
-     * Retieve sensor data from the mqtt server
-     */
-    public void retrieveData() {
-
-        // subscribe to all topics under sensor
-        mqtt.subscribeToTopic(client, "certificate/#");
-        mqtt.subscribeToTopic(client, "sensor/#");
-
-        mqtt.messageReceived(client);
-
-        Task task = new Task<Void>() {
-            @Override
-            public Void call() {
-                while (true) {
-                    if (isCancelled()) {
-                        break;
-                    }
-                    try {
-                        // used to update display with data retieved from mqtt server
-                        Thread.sleep(100);
-                        Platform.runLater(new Runnable() {
-                            @Override
-                            public void run() {
-                                try {
-                                    handleResult(mqtt.getResult());
-                                } catch (IOException e) {
-                                    e.printStackTrace();
-                                }
-
-                            }
-                        });
-                    } catch (Exception e) {
-
-                    }
-                }
-                return null;
-            }
-        };
-        new Thread(task).start();
-    }
-
-    /**
-     * Parse data accordingly and send to appropriate methods to be displayed
-     * 
-     * @param result
-     */
-    private void handleResult(String result) throws IOException {
-        try {
-            if (!result.equals("")) {
-                // parse data to get topic and info
-                String[] informations = result.split("'");
-                JSONObject json = new JSONObject(informations[1]);
-                String[] topics = informations[0].split("/");
-                if (json.has("certificate")) {
-                    String user = topics[1];
-                    String certString = json.get("certificate").toString();
-
-                    InputStream is = new ByteArrayInputStream(Base64.getDecoder().decode(certString));
-                    CertificateFactory cf = CertificateFactory.getInstance("X.509");
-                    X509Certificate cert = (X509Certificate) cf.generateCertificate(is);
-                    mqtt.getKeyStore().setCertificateEntry(user, cert);
-                    // TODO: Save Keystore into a file
-                }
-                // check sensor type and display accordingly
-                if (topics[0].equals("sensor")) {
-                    String sensorType = topics[1];
-                    String user = topics[2];
-                    PublicKey publicKey = null;
-
-                    if(mqtt.getKeyStore().getCertificate(user) == null){
-                        System.out.println("No matching certificate for user " + user);
-                    } else {
-                        Certificate cert = mqtt.getKeyStore().getCertificate(user);
-                        publicKey = cert.getPublicKey();
-                        // Add user public key to public key collection
-                        this.storedUsersPublicKeys.put(user, publicKey);
-                    }
-                    // Update user tiles with MQTT information and signature verificction using user public key
-                    switch (sensorType) {
-                        case "buzzer":
-                            sensorTimeUpdate(json, user, (PublicKey)this.storedUsersPublicKeys.get(user) , "buzzer");
-                            break;
-                        case "motion":
-                            sensorTimeUpdate(json, user, (PublicKey)this.storedUsersPublicKeys.get(user), "motion");
-                            break;
-                        case "humidity":
-                            sensorAmbientUpdate(json, user, (PublicKey)this.storedUsersPublicKeys.get(user));
-                            break;
-                        default:
-                            break;
-                    }
-                } else {
-
-                }
-            }
-
-        } catch (Exception e) {
-        }
-
-    }
-    /**
-     * Performs signature verification for ambient information and update corresponding user tiles
-     * @param json
-     * @param user
-     * @param publicKey
-     */
-    private void sensorAmbientUpdate(JSONObject json, String user, PublicKey publicKey) throws NoSuchAlgorithmException,
-            NoSuchProviderException, InvalidKeyException, UnsupportedEncodingException, SignatureException {
-        byte[] temperatureSignature = Base64.getDecoder().decode(json.get("signedTemp").toString());
-        byte[] humiditySignature = Base64.getDecoder().decode(json.get("signedHum").toString());
-        double temperature = Double.parseDouble(json.get("temperature").toString());
-        double humidity = Double.parseDouble(json.get("humidity").toString());
-
-        boolean temperatureCheck = this.instance.verifySignature(temperatureSignature, publicKey,
-                "SHA256withECDSA", json.get("temperature").toString());
-        boolean humidityCheck = this.instance.verifySignature(humiditySignature, publicKey,
-                "SHA256withECDSA", json.get("humidity").toString());
-        if (temperatureCheck) {
-            updateTempHum(user, temperature, humidity);
-        }
-    }
-    /**
-     * Performs signature verification for time and update corresponding user tiles
-     * @param json
-     * @param user
-     * @param publicKey
-     * @param type
-     */
-    private void sensorTimeUpdate(JSONObject json, String user, PublicKey publicKey, String type)
-            throws NoSuchAlgorithmException,
-            NoSuchProviderException, InvalidKeyException, UnsupportedEncodingException, SignatureException {
-        byte[] signatureMotionTimeBytes = Base64.getDecoder().decode(json.get("signedTime").toString());
-        boolean sensorTimeCheck = this.instance.verifySignature(signatureMotionTimeBytes, publicKey,
-                "SHA256withECDSA", json.get("time").toString());
-        if (sensorTimeCheck) {
-            String sensorDate = json.get("time").toString().substring(1, 10);
-            String sensorTime = json.get("time").toString().substring(11, 22);
-            String sensorTimestamp = sensorDate + " | " + sensorTime;
-            if (type.equals("buzzer")) {
-                updateTimeTile(user, sensorTimestamp, 6, 7, 8);
-            } else if (type.equals("motion")) {
-                updateTimeTile(user, sensorTimestamp, 9, 10, 11);
-            }
-        }
-    }
-
     /**
      * Update corresponding time tiles for buzzer and motion sensor
      * 
      * @param user
      * @param time
      */
-    private void updateTimeTile(String user, String time, int firstRow, int secondRow, int thirdRow) {
+    public void updateTimeTile(String user, String time, int firstRow, int secondRow, int thirdRow) {
         if (user.equals("johnny")) {
             tiles.get(firstRow).setDescription(time);
         } else if (user.equals("alexander")) {
@@ -384,7 +237,7 @@ public class FXDashboard extends HBox {
      * @param user
      * @param time
      */
-    private void updateTempHum(String user, double temperature, double humidity) {
+    public void updateTempHum(String user, double temperature, double humidity) {
         if (user.equals("johnny")) {
             tiles.get(0).setValue(temperature);
             tiles.get(1).setValue(humidity);
