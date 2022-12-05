2a5b73b78d8ddee6d39b87c32427f7d03e99c420
johnnyhoaang <johnny.hoang@dawsoncollege.qc.ca>
2022-12-04 21:54:52 -0500

    Refactor code

--- src/main/java/com/mycompany/mqtt/client/app/FXDashboard.java
+++ src/main/java/com/mycompany/mqtt/client/app/FXDashboard.java
@@ -4,30 +4,29 @@ import java.io.ByteArrayInputStream;
 import java.io.Console;
 import java.io.IOException;
 import java.io.InputStream;
-import java.nio.charset.StandardCharsets;
-import java.security.KeyFactory;
+import java.io.UnsupportedEncodingException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
 import java.security.PublicKey;
+import java.security.SignatureException;
 import java.security.cert.Certificate;
 import java.security.cert.CertificateFactory;
 import java.security.cert.X509Certificate;
-import java.security.spec.X509EncodedKeySpec;
 import java.util.ArrayList;
 import java.util.Base64;
-import java.util.Scanner;
 
 import eu.hansolo.tilesfx.Tile;
 import eu.hansolo.tilesfx.TileBuilder;
 import eu.hansolo.tilesfx.Tile.ImageMask;
 import eu.hansolo.tilesfx.Tile.SkinType;
 import javafx.geometry.Pos;
-import javafx.scene.image.Image;
 import javafx.scene.layout.HBox;
 import javafx.application.Platform;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.TextAlignment;
 import javafx.concurrent.Task;
 
-import com.hivemq.client.internal.mqtt.datatypes.MqttTopicFilterImplBuilder.Base;
 import com.hivemq.client.mqtt.mqtt5.Mqtt5BlockingClient;
 import org.json.*;
 
@@ -42,18 +41,19 @@ public class FXDashboard extends HBox {
     private int PREF_WIDTH = 300;
     private int PREF_HEIGHT = 200;
     Console console = System.console();
-    public FXDashboard(MqttRun mqtt, String topicUser){
+
+    public FXDashboard(MqttRun mqtt, String topicUser) {
         this.instance = new LogicHandler();
         String ksPath = console.readLine("Enter Keystore path: ");
-        char [] password = console.readPassword("Enter Keystore password: ");
+        char[] password = console.readPassword("Enter Keystore password: ");
         var ks = this.instance.loadKeystore(ksPath, password);
         this.mqtt = mqtt;
         mqtt.setKeyStore(ks);
         this.client = mqtt.run();
-        
+
         try {
             this.buildScreen();
-        } catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
@@ -67,7 +67,7 @@ public class FXDashboard extends HBox {
             if (i == 0) {
                 titleTemp = "Temperature - Johnny";
                 titleHum = "Humidity - Johnny";
-            } else if(i == 1) {
+            } else if (i == 1) {
                 titleTemp = "Temperature - Alexander";
                 titleHum = "Humidity - Alexander";
             } else {
@@ -75,28 +75,28 @@ public class FXDashboard extends HBox {
                 titleHum = "Humidity - Katharina";
             }
             var gaugeTile = TileBuilder.create()
-                .skinType(Tile.SkinType.GAUGE)
-                .prefSize(PREF_WIDTH/2, PREF_HEIGHT)
-                .title(titleTemp)
-                .text("Temperature")
-                .unit("Celsius")
-                .textVisible(true)
-                .textAlignment(TextAlignment.LEFT)
-                .value(0)
-                .threshold(75)
-                .animated(true)
-                .build();
+                    .skinType(Tile.SkinType.GAUGE)
+                    .prefSize(PREF_WIDTH / 2, PREF_HEIGHT)
+                    .title(titleTemp)
+                    .text("Temperature")
+                    .unit("Celsius")
+                    .textVisible(true)
+                    .textAlignment(TextAlignment.LEFT)
+                    .value(0)
+                    .threshold(75)
+                    .animated(true)
+                    .build();
 
             tiles.add(gaugeTile);
 
             var percentageTile = TileBuilder.create()
-                .skinType(Tile.SkinType.PERCENTAGE)
-                .prefSize(PREF_WIDTH/2, PREF_HEIGHT)
-                .title(titleHum)
-                .unit("Percent")
-                .description("Humidity")
-                .maxValue(60)
-                .build();
+                    .skinType(Tile.SkinType.PERCENTAGE)
+                    .prefSize(PREF_WIDTH / 2, PREF_HEIGHT)
+                    .title(titleHum)
+                    .unit("Percent")
+                    .description("Humidity")
+                    .maxValue(60)
+                    .build();
 
             tiles.add(percentageTile);
         }
@@ -104,14 +104,14 @@ public class FXDashboard extends HBox {
         // group temperature and humidity tiles together to look like one tile
         var num = 0;
         for (int i = 0; i < 3; i++) {
-            
+
             var row = new HBox();
 
-            if(i == 1)
+            if (i == 1)
                 num = 2;
-            if(i == 2) 
+            if (i == 2)
                 num = 4;
-            for (int j = num; j < num+2; j++) {
+            for (int j = num; j < num + 2; j++) {
                 row.getChildren().add(tiles.get(j));
             }
             hboxs.add(row);
@@ -127,21 +127,21 @@ public class FXDashboard extends HBox {
             String title = "";
             if (i == 0) {
                 title = prefix + "Johnny";
-            } else if(i == 1) {
-                title = prefix +"Alexander";
+            } else if (i == 1) {
+                title = prefix + "Alexander";
             } else {
                 title = prefix + "Katharina";
             }
             var image = TileBuilder.create()
-                .skinType(SkinType.IMAGE)
-                .prefSize(PREF_WIDTH, PREF_HEIGHT)
-                .title(title)
-                .image(null)
-                .imageMask(ImageMask.RECTANGULAR)
-                .text("Image taken when motion is detected")
-                .textAlignment(TextAlignment.LEFT)
-                .textVisible(true)
-                .build();
+                    .skinType(SkinType.IMAGE)
+                    .prefSize(PREF_WIDTH, PREF_HEIGHT)
+                    .title(title)
+                    .image(null)
+                    .imageMask(ImageMask.RECTANGULAR)
+                    .text("Image taken when motion is detected")
+                    .textAlignment(TextAlignment.LEFT)
+                    .textVisible(true)
+                    .build();
 
             tiles.add(image);
         }
@@ -158,14 +158,14 @@ public class FXDashboard extends HBox {
         // indicate where to start reading the tile list
         num = 6;
         for (int i = 0; i < 3; i++) {
-            
+
             var column = new VBox();
-            if (i == 1) 
+            if (i == 1)
                 num = 9;
-            if(i == 2) 
+            if (i == 2)
                 num = 12;
-            
-            for (int j = num; j < num+3; j++) {
+
+            for (int j = num; j < num + 3; j++) {
                 column.getChildren().add(tiles.get(j));
             }
             column.setSpacing(5);
@@ -179,33 +179,35 @@ public class FXDashboard extends HBox {
         }
         this.setSpacing(5);
 
-        retrieveData(); 
+        retrieveData();
     }
 
     /**
      * Create text tiles for timestamps for each member
+     * 
      * @param prefix
      * @param desc
      */
     private void createTextTiles(String prefix, String desc) {
         var title = "";
-        for (int i = 0; i < 3; i++) {;
+        for (int i = 0; i < 3; i++) {
+            ;
             if (i == 0) {
                 title = prefix + "Johnny";
-            } else if(i == 1) {
+            } else if (i == 1) {
                 title = prefix + "Alexander";
             } else {
                 title = prefix + "Katharina";
             }
             var buzzer = TileBuilder.create()
-                .skinType(SkinType.TEXT)
-                .prefSize(PREF_WIDTH, PREF_HEIGHT)
-                .title(title)
-                .text(desc)
-                .description("Timestamp")
-                .descriptionAlignment(Pos.BASELINE_LEFT)
-                .textVisible(true)
-                .build();
+                    .skinType(SkinType.TEXT)
+                    .prefSize(PREF_WIDTH, PREF_HEIGHT)
+                    .title(title)
+                    .text(desc)
+                    .description("Timestamp")
+                    .descriptionAlignment(Pos.BASELINE_LEFT)
+                    .textVisible(true)
+                    .build();
 
             tiles.add(buzzer);
         }
@@ -214,34 +216,36 @@ public class FXDashboard extends HBox {
     /**
      * Retieve sensor data from the mqtt server
      */
-    public void retrieveData(){
+    public void retrieveData() {
 
         // subscribe to all topics under sensor
         mqtt.subscribeToTopic(client, "certificate/#");
-        mqtt.subscribeToTopic(client,"sensor/#");
+        mqtt.subscribeToTopic(client, "sensor/#");
 
         mqtt.messageReceived(client);
-       
+
         Task task = new Task<Void>() {
-            @Override public Void call() {
-                while(true){
+            @Override
+            public Void call() {
+                while (true) {
                     if (isCancelled()) {
-                       break;
+                        break;
                     }
                     try {
                         // used to update display with data retieved from mqtt server
                         Thread.sleep(100);
-                        Platform.runLater(new Runnable(){
-                        @Override
-                        public void run(){
-                            try {
-                                handleResult(mqtt.getResult());
-                            } catch(IOException e){
-                                e.printStackTrace();
+                        Platform.runLater(new Runnable() {
+                            @Override
+                            public void run() {
+                                try {
+                                    handleResult(mqtt.getResult());
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                }
+
                             }
-                                             
-                        }});
-                    } catch(Exception e){
+                        });
+                    } catch (Exception e) {
 
                     }
                 }
@@ -253,15 +257,16 @@ public class FXDashboard extends HBox {
 
     /**
      * Parse data accordingly and send to appropriate methods to be displayed
+     * 
      * @param result
      */
-    private void handleResult(String result) throws IOException{
+    private void handleResult(String result) throws IOException {
         try {
             // parse data to get topic and info
-            String [] informations = result.split("'");
+            String[] informations = result.split("'");
             JSONObject json = new JSONObject(informations[1]);
-            String []topics = informations[0].split("/");
-            if(json.has("certificate")){
+            String[] topics = informations[0].split("/");
+            if (json.has("certificate")) {
                 String user = topics[1];
                 String certString = json.get("certificate").toString();
 
@@ -271,87 +276,94 @@ public class FXDashboard extends HBox {
                 mqtt.getKeyStore().setCertificateEntry(user, cert);
             }
             // check sensor type and display accordingly
-            if(topics[0].equals("sensor")){
+            if (topics[0].equals("sensor")) {
                 String sensorType = topics[1];
                 String user = topics[2];
-                PublicKey publicKey = null; 
-                
-                if(user.equals("johnny")){
+                PublicKey publicKey = null;
+
+                if (user.equals("johnny")) {
                     Certificate cert = mqtt.getKeyStore().getCertificate(user);
                     publicKey = cert.getPublicKey();
                 }
-                switch(sensorType){
+                switch (sensorType) {
                     case "buzzer":
-                        byte [] signatureBytes  = Base64.getDecoder().decode(json.get("signedTime").toString());
-                        boolean dateCheck = this.instance.verifySignature(signatureBytes, publicKey,"SHA256withECDSA",json.get("time").toString());
-                        if(dateCheck){
-                            String buzzerDate = json.get("time").toString().substring(0, 10);
-                        String buzzerTime = json.get("time").toString().substring(11, 22);
-                        String buzzerTimestamp = buzzerDate + " | " + buzzerTime;
-                        
-                        updateBuzzerTile(user, buzzerTimestamp);
-                        } 
+                        sensorTimeUpdate(json, user, publicKey, "buzzer");
                         break;
                     case "motion":
-                        String motionDate = json.get("time").toString().substring(1, 10);
-                        String motionTime = json.get("time").toString().substring(11, 22);
-                        String motionTimestamp = motionDate + " | " + motionTime;
-                        updateMotionTile(user, motionTimestamp);
+                        sensorTimeUpdate(json, user, publicKey, "motion");
                         break;
                     case "humidity":
-                        double temperature = Double.parseDouble(json.get("temperature").toString());
-                        double humidity = Double.parseDouble(json.get("humidity").toString());
-                        updateTempHum(user, temperature, humidity);
+                        sensorAmbientUpdate(json, user, publicKey);
                         break;
                     default:
                         break;
                 }
             } else {
-                
+
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
 
     }
-    
-    /**
-     * Update corresponding tiles for buzzer sensor
-     * @param user
-     * @param time
-     */
-    private void updateBuzzerTile(String user, String time) {
-        if(user.equals("johnny")){
-            tiles.get(6).setDescription(time);
-        } else if(user.equals("alexander")) {
-            tiles.get(7).setDescription(time);
-        } else if(user.equals("katharina")) {
-            tiles.get(8).setDescription(time);
+
+    private void sensorAmbientUpdate(JSONObject json, String user, PublicKey publicKey) throws NoSuchAlgorithmException,
+            NoSuchProviderException, InvalidKeyException, UnsupportedEncodingException, SignatureException {
+        byte[] temperatureSignature = Base64.getDecoder().decode(json.get("temperature").toString());
+        byte[] humiditySignature = Base64.getDecoder().decode(json.get("humidity").toString());
+        double temperature = Double.parseDouble(json.get("temperature").toString());
+        double humidity = Double.parseDouble(json.get("humidity").toString());
+        boolean humidityCheck = this.instance.verifySignature(temperatureSignature, publicKey,
+                "SHA256withECDSA", json.get("time").toString());
+        boolean temperatureCheck = this.instance.verifySignature(humiditySignature, publicKey,
+                "SHA256withECDSA", json.get("time").toString());
+        if (humidityCheck && temperatureCheck) {
+            updateTempHum(user, temperature, humidity);
+        }
+    }
+
+    private void sensorTimeUpdate(JSONObject json, String user, PublicKey publicKey, String type)
+            throws NoSuchAlgorithmException,
+            NoSuchProviderException, InvalidKeyException, UnsupportedEncodingException, SignatureException {
+        byte[] signatureMotionTimeBytes = Base64.getDecoder().decode(json.get("signedTime").toString());
+        boolean sensorTimeCheck = this.instance.verifySignature(signatureMotionTimeBytes, publicKey,
+                "SHA256withECDSA", json.get("time").toString());
+        if (sensorTimeCheck) {
+            String sensorDate = json.get("time").toString().substring(1, 10);
+            String sensorTime = json.get("time").toString().substring(11, 22);
+            String sensorTimestamp = sensorDate + " | " + sensorTime;
+            if (type.equals("buzzer")) {
+                updateTimeTile(user, sensorTimestamp,6,7,8);
+            } else if (type.equals("motion")) {
+                updateTimeTile(user, sensorTimestamp,9,10,11);
+            }
         }
     }
 
     /**
-     * Update corresponding tiles for motion sensor
+     * Update corresponding time tiles for buzzer and motion sensor
+     * 
      * @param user
      * @param time
      */
-    private void updateMotionTile(String user, String time) {
-        if(user.equals("johnny")){
-            tiles.get(9).setDescription(time);
-        } else if(user.equals("alexander")) {
-            tiles.get(10).setDescription(time);
-        } else if(user.equals("katharina")) {
-            tiles.get(11).setDescription(time);
+    private void updateTimeTile(String user, String time, int first, int second, int third) {
+        if (user.equals("johnny")) {
+            tiles.get(first).setDescription(time);
+        } else if (user.equals("alexander")) {
+            tiles.get(second).setDescription(time);
+        } else if (user.equals("katharina")) {
+            tiles.get(third).setDescription(time);
         }
     }
 
     /**
      * Update corresponding tiles for temperature/humididty sensor
+     * 
      * @param user
      * @param time
      */
     private void updateTempHum(String user, double temperature, double humidity) {
-        if(user.equals("johnny")){
+        if (user.equals("johnny")) {
             tiles.get(0).setValue(temperature);
             tiles.get(1).setValue(humidity);
         } else if (user.equals("alexander")) {
